3378b8462a8b62f224a4d87267796d946e2114fc
Jo4ovlm <jo4ovlm@gmail.com>
2025-03-31 11:57:48 -0300

    Implementação do clipping agora com um Algoritmo mais otimizado

--- src/Linha.java
+++ src/Linha.java
@@ -18,19 +18,8 @@ public class Linha {
 
     public void desenhase(PrototipoTela p) { // Aqui eu quero fazer um for no array de pixeisBresenham e usar a função desenha pixel la do PrototipoTela
         for(int[] pixel : this.pixeisBresenham){
-            if(!detectaClipping(p, pixel)){ // so desenha se nao detectar o clipping
                 p.desenhaPixel(pixel[0], pixel[1], 0, 0, 0);
-            }
         }
-    }
-
-    public boolean detectaClipping(PrototipoTela p, int[] pixel){
-        if(p.getLimiteEsquerdo() > pixel[0] || pixel[0] > p.getLimiteDireito()){ // Se ele for mais a esquerda que o limite esquerdo ou mais a direita que o limite direito
-            return true; // detecta clipping
-        } else if (p.getLimiteSuperior() > pixel[1] || pixel[1] > p.getLimiteInferior()) { // Se ele for mais acima que o limite superior ou mais abaixo que o limite inferior
-            return true; // detecta clipping
-        }
-        return false; // sem clipping
     }
 
 }
--- src/PrototipoTela.java
+++ src/PrototipoTela.java
@@ -18,11 +18,18 @@ public class PrototipoTela extends JPanel implements Runnable{
     private int mouseX;
     private int mouseY;
 
+	//Codigo das regioes Clipping
+	static final int INSIDE = 0; // 0000
+	static final int LEFT = 1; // 0001
+	static final int RIGHT = 2; // 0010
+	static final int BOTTOM = 4; // 0100
+	static final int TOP = 8; // 1000
+
 	//Limites tela desenhavel
-	private final int limiteSuperior = 50; // Superior e inferior é eixo Y
-	private final int limiteInferior = 550;
-	private final int limiteEsquerdo = 50; // Esquerdo e direito é eixo X
-	private final int limiteDireito =  750;
+	private final int y_min = 50; // Superior e inferior é eixo Y
+	private final int y_max = 550;
+	private final int x_min = 50; // Esquerdo e direito é eixo X
+	private final int x_max =  750;
 
     private Ponto p1linhadesenhando = null;
     private ArrayList<Linha> listaDeLinhas = new ArrayList<>();
@@ -33,20 +40,20 @@ public class PrototipoTela extends JPanel implements Runnable{
 	private byte[] bufferDeVideo;
 
 	// Getter Variaveis de limite de area desenhavel
-	public int getLimiteSuperior() {
-		return limiteSuperior;
+	public int getY_min() {
+		return y_min;
 	}
 
-	public int getLimiteInferior() {
-		return limiteInferior;
+	public int getY_max() {
+		return y_max;
 	}
 
-	public int getLimiteEsquerdo() {
-		return limiteEsquerdo;
+	public int getX_min() {
+		return x_min;
 	}
 
-	public int getLimiteDireito() {
-		return limiteDireito;
+	public int getX_max() {
+		return x_max;
 	}
 
 	//Config teste de tela
@@ -90,9 +97,11 @@ public class PrototipoTela extends JPanel implements Runnable{
 					p1linhadesenhando = new Ponto(clickX, clickY);
 				}else {
 					Ponto p2 = new Ponto(clickX, clickY);
-					Linha linha = new Linha(p1linhadesenhando.x,p1linhadesenhando.y, p2.x,p2.y);
-					linha.pixeisBresenham = Bresenham.bresenham(linha.a.x,linha.a.y, linha.b.x, linha.b.y);
-					listaDeLinhas.add(linha);
+					Linha linha = clipping(p1linhadesenhando.x,p1linhadesenhando.y, p2.x,p2.y);
+					if(linha.a.x != -1){ // Verifica se a linha é invalida (2 pontos fora da area desenhavel e não passa por dentro em nenhum momento)
+						linha.pixeisBresenham = Bresenham.bresenham(linha.a.x,linha.a.y, linha.b.x, linha.b.y);
+						listaDeLinhas.add(linha);
+					}
 					p1linhadesenhando = null;
 				}
             }
@@ -132,10 +141,10 @@ public class PrototipoTela extends JPanel implements Runnable{
 		g.fillRect(0, 0, larguraTela, alturaTela);
 
 		//Desenha borda usando os limites definidos la em cima de parametro
-		desenhaLinhaHorizontal(limiteEsquerdo, limiteSuperior,limiteDireito - limiteEsquerdo);
-		desenhaLinhaHorizontal(limiteEsquerdo,limiteInferior,limiteDireito - limiteEsquerdo);
-		desenhaLinhaVertical(limiteEsquerdo, limiteSuperior,limiteInferior - limiteSuperior);
-		desenhaLinhaVertical(limiteDireito, limiteSuperior, limiteInferior - limiteSuperior);
+		desenhaLinhaHorizontal(x_min, y_min, x_max - x_min);
+		desenhaLinhaHorizontal(x_min, y_max, x_max - x_min);
+		desenhaLinhaVertical(x_min, y_min, y_max - y_min);
+		desenhaLinhaVertical(x_max, y_min, y_max - y_min);
 		
 		g.setColor(Color.black);
 		for(int i = 0; i < listaDeLinhas.size();i++) {
@@ -189,6 +198,74 @@ public class PrototipoTela extends JPanel implements Runnable{
 		bufferDeVideo[pospix+2] = (byte)(g&0xff);
 		bufferDeVideo[pospix+3] = (byte)(r&0xff);
 	
+	}
+
+	// Função pra detectar se tem clipping, e em que região ocorre
+	public int detectaClipping(int x, int y){
+		int cod = INSIDE; // Por padrao considera que ta dentro da tela
+
+		if (x < x_min) // Ve se ta na esquerda da tela
+			cod |= LEFT;
+		else if (x > x_max) //  Ve se ta na direita da tela
+			cod |= RIGHT;
+		if (y < y_min) //  Ve acima da tela
+			cod |= BOTTOM;
+		else if (y > y_max) // Ve se ta abaixo da tela
+			cod |= TOP;
+
+		return cod;
+	}
+
+	public Linha clipping(int x1, int y1, int x2, int y2){
+		int cod1 = detectaClipping(x1,y1); // Ve se algum dos 2 pontos esta fora da tela
+		int cod2 = detectaClipping(x2,y2);
+
+		while(true){
+			if(cod1 == INSIDE && cod2 == INSIDE){ // Ve se os dois ja tao dentro da tela
+				return new Linha(x1,y1,x2,y2);
+			}
+			else if((cod1 & cod2) != INSIDE){ // Ve se os dois estão fora da tela e não passa por dentro dela
+				return new Linha(-1,-1,-1,-1); // Retorna uma Linha invalida pra verificação no retorno
+			}
+			else{
+				int codFora; // Codigo da região do ponto fora da tela
+				int x = 0, y = 0;
+
+				if(cod1 != INSIDE){
+					codFora = cod1;
+				}else{
+					codFora = cod2;
+				}
+
+				if((codFora & TOP) != 0){ // Se estiver acima da tela
+					x = x1 + (x2 - x1)* (y_max - y1) / (y2 - y1);
+					y = y_max;
+				}else if((codFora & BOTTOM) != 0){ // Se estiver abaixo
+					x = x1 + (x2 - x1) * (y_min - y1) / (y2 - y1);
+					y = y_min;
+				}else if((codFora & RIGHT) != 0){ // Se estiver a direita
+					y = y1 + (y2 - y1) * (x_max - x1) / (x2 - x1);
+					x = x_max;
+				}
+				else if((codFora & LEFT) != 0){ // Se estiver a esquerda
+					y = y1 + (y2 - y1) * (x_min - x1) / (x2 - x1);
+					x = x_min;
+				}
+
+				if (codFora == cod1) {
+					x1 = x;
+					y1 = y;
+					cod1 = detectaClipping(x1, y1);
+				}
+				else {
+					x2 = x;
+					y2 = y;
+					cod2 = detectaClipping(x2, y2);
+				}
+			}
+
+		}
+
 	}
 	
     //Aqui é onde é iniciado la pelo main
